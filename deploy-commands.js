f9bb1e83a33b33dacf546d273b73d7f3248e9cc3
marcos-aparicio <marcos.aparicio593@gmail.com>
2025-01-08 10:37:42 -0500

    refactoring deploy-commands.js so that is not dependent on guildId from config.json

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,7 +2,7 @@ const fs = require("node:fs");
 const path = require("node:path");
 const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord.js");
-const { clientId, guildId, token } = require("./config.json");
+const { clientId, token } = require("./config.json");
 
 const { preloadData } = require("./data.js");
 
@@ -24,7 +24,7 @@ const setUpCommands = async () => {
   const rest = new REST({ version: "10" }).setToken(token);
 
   rest
-    .put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+    .put(Routes.applicationCommands(clientId), { body: commands })
     .then(() => console.log("Successfully registered application commands."))
     .catch(console.error);
 };
